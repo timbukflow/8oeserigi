916aca7031da5588f637fcc328526e74196eef0f
Ivo Schwizer <ivoschwizer@gmail.com>
2021-11-18 16:56:39 +0100

    grundrisse

---
+++ grundrisse-preise.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!--[if IE 7]>	 <html class="no-js lt-ie9 lt-ie8" lang="de"><![endif]-->
+<!--[if IE 8]>	 <html class="no-js lt-ie9" lang="de"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="de"><!--<![endif]-->
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# place: http://ogp.me/ns/place#">
+<title>8 &Ouml;serigi | Grundrisse & Preise</title>
+<meta name="description" content="8 Öserigi | Grundrisse & Preise"/>
+<?php include 'head.php'; ?>
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <section class="gcontainer">
+      <h1>Grundrisse & Preise</h1>
+      <article class="gbereich">
+        <div class="ghaus">
+          <img class="hausimg" src="img/8-oeserigi-grundriss-haus-a-1-4.svg" alt="8-oeserigi-grundriss-haus-a-1-4">
+          <h3>Haus A1</h3>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+        </div>
+        <div class="ghaus">
+          <img class="hausimg" src="img/8-oeserigi-grundriss-haus-a-1-4.svg" alt="8-oeserigi-grundriss-haus-a-1-4">
+          <h3>Haus A1</h3>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+        </div>
+        <div class="ghaus">
+          <img class="hausimg" src="img/8-oeserigi-grundriss-haus-a-1-4.svg" alt="8-oeserigi-grundriss-haus-a-1-4">
+          <h3>Haus A1</h3>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+        </div>
+        <div class="ghaus">
+          <img class="hausimg" src="img/8-oeserigi-grundriss-haus-a-1-4.svg" alt="8-oeserigi-grundriss-haus-a-1-4">
+          <h3>Haus A1</h3>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+        </div>
+        <div class="ghaus">
+          <img class="hausimg" src="img/8-oeserigi-grundriss-haus-a-1-4.svg" alt="8-oeserigi-grundriss-haus-a-1-4">
+          <h3>Haus A1</h3>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+          <h4><a href="downloads/stepcube-grundrisse.pdf" download="Stepcube-Grundrisse"><img src="img/download-icon.svg" width="11" height="11" alt="download icon"> Grundrisse Haus A1</a></h4>
+        </div>
+      </article>
+    </section>
+
+    <?php include 'footer.php'; ?>
+    <?php include 'script.php'; ?>
+</body>
+</html>
