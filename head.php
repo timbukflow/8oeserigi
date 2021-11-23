55db8f38bd7d691ca525074a38da0894e877cb44
Ivo Schwizer <ivoschwizer@gmail.com>
2021-11-23 14:51:32 +0100

    favicon

--- head.php
+++ head.php
@@ -3,28 +3,20 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="robots" content="all">
 <meta name="robots" content="index, follow" />
-<meta name="author" content="Stepcube Romanshorn">
+<meta name="author" content="8 Öserigi | Appenzell Innerrhoden">
 <meta http-equiv="cache-control" content="max-age=0" />
 <meta http-equiv="cache-control" content="no-cache" />
 <meta http-equiv="expires" content="0" />
 <meta http-equiv="pragma" content="no-cache" />
 
-<link rel="apple-touch-icon" sizes="57x57" href="/img/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/img/favicon/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/img/favicon/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/img/favicon/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/img/favicon/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="/img/favicon/android-icon-192x192.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="/img/favicon/favicon-96x96.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
-<link rel="manifest" href="/img/favicon/manifest.json">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/img/favicon/ms-icon-144x144.png">
+<link rel="manifest" href="/img/favicon/site.webmanifest">
+<link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#b3a280">
+<link rel="shortcut icon" href="/img/favicon/favicon.ico">
+<meta name="msapplication-TileColor" content="#333333">
+<meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
 
 <meta property="og:title" content="8 Öserigi | Appenzell Innerrhoden" />
